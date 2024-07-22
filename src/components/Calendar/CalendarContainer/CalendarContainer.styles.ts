781651d88d9219e4324db87c6b4f45e20028c857
Ben Freudman <bfreud94@gmail.com>
2024-07-21 21:21:44 -0400

    updated calendar background color

--- src/components/Calendar/CalendarContainer/CalendarContainer.styles.ts
+++ src/components/Calendar/CalendarContainer/CalendarContainer.styles.ts
@@ -1,9 +1,10 @@
 import { CalendarContainerStyles } from './CalendarContainer.types'
 
 const makeStyles = (): CalendarContainerStyles => ({
-	container: {
+	container: ({
+		backgroundColor: '#FFF0BA',
 		border: '1px solid black'
-	}
+	})
 })
 
 export default makeStyles
