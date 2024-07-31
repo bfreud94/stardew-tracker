58f9560e4e3b8368f9384fdcb86321bdd5efb32b
Ben Freudman <bfreud94@gmail.com>
2024-07-30 23:41:16 -0400

    changed note modal font

--- src/components/Calendar/NoteModal/NoteInput/NoteInput.styles.ts
+++ src/components/Calendar/NoteModal/NoteInput/NoteInput.styles.ts
@@ -2,6 +2,8 @@ import { NoteInputStyles } from './NoteInput.types'
 
 const makeStyles = (): NoteInputStyles => ({
 	noteInput: {
+		fontFamily: 'SVFont',
+		fontSize: '18px',
 		height: '50px',
 		marginTop: '20px',
 		paddingLeft: '5px',
